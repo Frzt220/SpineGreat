5f920db32d24d7a86a925babdf8475c31980a942
Asitaka.Xiao <asitaka.xiao@mechanist.co>
2022-12-01 10:57:55 +0800

    首次提交

---
+++ Assets/InitBatchSpine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitBatchSpine : MonoBehaviour
+{
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    void Init() {
+        for (int i = 0; i < 1000; i++) {
+            GameObject child = GameObject.Instantiate(prefab);
+            child.transform.name = i.ToString();
+            child.transform.localPosition = new Vector3(-8 + (i % 32)/2.0f, 3 - (i % 10), 0);
+        }
+
+    }
+
+}
